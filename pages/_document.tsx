0c83715ecbe8ec723ff6e978bb99723716be930b
Haobo Zhang <haobozhang9081@gmail.com>
2021-04-03 22:03:12 -0700

    now for all icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,10 +26,13 @@ export default class _Document extends Document {
 		return <Html lang='en'>
 			<Head>
 				<link rel='manifest' href='manifest.json'/>
-				<link rel="apple-touch-icon" href="icon-192x192.png"/>
-				<link rel="apple-touch-icon" sizes="256x256" href="icon-256x256.png"/>
-				<link rel="apple-touch-icon" sizes="384x384" href="icon-384x384.png"/>
-				<link rel="apple-touch-icon" sizes="512x512" href="icon-512x512.png"/>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+				<link rel="manifest" href="/site.webmanifest"/>
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#039be5"/>
+				<meta name="msapplication-TileColor" content="#da532c"/>
+				<meta name="theme-color" content="#ffffff"/>
 				
 				<link
 					rel='stylesheet'
