f630cf734e5774c7557e6be0e40641a813d6660d
Haobo Zhang <haobozhang9081@gmail.com>
2021-04-04 23:45:12 -0700

    updated packages and webpack 5

--- next.config.js
+++ next.config.js
@@ -6,9 +6,12 @@ const withBundleAnalyzer = bundleAnalyzer( {
 	enabled: process.env.ANALYZE === 'true'
 } );
 module.exports = withBundleAnalyzer( withPWA( {
-	pwa: {
+	pwa:    {
 		disable: process.env.NODE_ENV === 'development',
 		dest:    'public',
 		runtimeCaching
+	},
+	future: {
+		webpack5: true
 	}
 } ) );
