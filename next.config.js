987bf3ecd00ac2c30d49a134500619216d089aaa
Haobo Zhang <haobozhang9081@gmail.com>
2021-06-18 20:16:15 -0700

    switch to webpack 5

--- next.config.js
+++ next.config.js
@@ -14,10 +14,9 @@ module.exports = withBundleAnalyzer( withPWA( {
 		} ) );
 		return config;
 	},
-	pwa   : {
+	pwa: {
 		disable: process.env.NODE_ENV === 'development',
 		dest   : 'public',
 		runtimeCaching
-	},
-	future: { webpack5: process.env.NODE_ENV !== 'development' }
+	}
 } ) );
