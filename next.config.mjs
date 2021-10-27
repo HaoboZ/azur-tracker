94df6c76caa2c782f2f523829df90c599ba92abb
Haobo Zhang <haobozhang9081@gmail.com>
2021-10-26 22:33:33 -0700

    removed swcMinify since its broken

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,7 @@ export default withBundleAnalyzer( withPWA( {
 		);
 		return config;
 	},
-	swcMinify: process.env.NODE_ENV !== 'development',
-	pwa      : {
+	pwa: {
 		disable: process.env.NODE_ENV === 'development',
 		dest   : 'public'
 	}
