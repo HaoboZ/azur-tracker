f6101f59b05a5e09d91ee2be838d1ba779fc7b60
Haobo Zhang <haobozhang9081@gmail.com>
2021-10-27 20:56:43 -0700

    fixed typing

--- src/lib/google/drive/getDrive.ts
+++ src/lib/google/drive/getDrive.ts
@@ -3,6 +3,7 @@ import { getToken } from 'next-auth/jwt';
 import { auth } from '../authInstance';
 
 export default async function getDrive( req ) {
+	// @ts-ignore
 	const { accessToken, refreshToken } = await getToken( { req, secret: process.env.SECRET } );
 	auth.setCredentials( { access_token: accessToken as string, refresh_token: refreshToken as string } );
 	return google.drive( { version: 'v3', auth } );
