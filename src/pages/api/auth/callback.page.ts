921894ed1bc2418e2ec724c3ce18b992dbaea7c5
Haobo Zhang <haobozhang9081@gmail.com>
2022-01-11 10:22:54 -0800

    added test api routes

---
+++ src/pages/api/auth/callback.page.ts
@@ -0,0 +1,18 @@
+import { NextApiHandler } from 'next';
+import { oAuthClient } from '../../../lib/firebase/oAuthClient';
+
+const callback: NextApiHandler = async ( req, res ) => {
+	try {
+		const { tokens } = await oAuthClient.getToken( req.query.code as string );
+		res.send( `<script defer>
+	window.onload = async () => {
+		await window.opener.authenticate( ${JSON.stringify( tokens )} );
+		window.close();
+	};
+</script>` );
+	} catch ( e ) {
+		return res.status( 400 ).send( String( e ) );
+	}
+};
+// noinspection JSUnusedGlobalSymbols
+export default callback;
---
+++ src/pages/api/auth/login.page.ts
@@ -0,0 +1,21 @@
+import { NextApiHandler } from 'next';
+import { oAuthClient } from '../../../lib/firebase/oAuthClient';
+
+const login: NextApiHandler = async ( req, res ) => {
+	try {
+		const url = oAuthClient.generateAuthUrl( {
+			access_type : 'offline',
+			prompt      : 'consent',
+			scope       : [
+				'https://www.googleapis.com/auth/userinfo.email',
+				'https://www.googleapis.com/auth/userinfo.profile'
+			],
+			redirect_uri: process.env.GOOGLE_CLIENT_REDIRECT_URI
+		} );
+		res.send( url );
+	} catch ( e ) {
+		res.status( 400 ).send( String( e ) );
+	}
+};
+// noinspection JSUnusedGlobalSymbols
+export default login;
