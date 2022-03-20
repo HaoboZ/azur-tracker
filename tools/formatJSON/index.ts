edfe36c4b132dde57b4ab5abcea8fda7a48944e6
Haobo Zhang <haobozhang9081@gmail.com>
2022-03-20 15:15:23 -0700

    updated neatjson config

--- tools/formatJSON/index.ts
+++ tools/formatJSON/index.ts
@@ -4,11 +4,4 @@ import neatJSON from './neatJSON';
 
 const file = fs.readFileSync( argv[ 2 ], { encoding: 'utf8' } ).toString();
 
-fs.writeFileSync( argv[ 2 ], neatJSON( JSON.parse( file ), {
-	wrap       : true,
-	indent     : '\t',
-	aligned    : true,
-	padding    : 1,
-	afterComma : 1,
-	afterColonN: 1
-} ) );
+fs.writeFileSync( argv[ 2 ], neatJSON( JSON.parse( file ), { aligned: true } ) );
--- tools/formatJSON/neatJSON.ts
+++ tools/formatJSON/neatJSON.ts
@@ -1,45 +1,72 @@
+import os from 'os';
+
 type Options = {
-	wrap?: boolean | number, // Maximum line width before wrapping. Use false to never wrap, true to always wrap. default: 80
-	indent?: string, // Whitespace used to indent each level when wrapping. default: " " (two spaces)
-	indentLast?: boolean, // Indent the closing bracket/brace for arrays and objects? default: false
-	short?: boolean, // Put opening brackets on the same line as the first value, closing brackets on the same line as the last? default: false
+	// Maximum line width before wrapping. Use false to never wrap, true to always wrap. default: 80
+	wrap?: boolean | number,
+	// Whitespace used to indent each level when wrapping. default: "\t" (tab)
+	indent?: string,
+	// Newline character(s) to use when wrapping. default: os.EOL
+	newline?: string,
+	// Indent the closing bracket/brace for arrays and objects? default: false
+	indentLast?: boolean,
+	// Put opening brackets on the same line as the first value, closing brackets on the same line as the last? default: false
 	// This causes the indent and indent_last options to be ignored, instead basing indentation on array and object padding.
-	sort?: boolean | ( ( a: string, b: string ) => number ), // Sort objects' keys in alphabetical order (true), or supply a lambda for custom sorting. default: false
+	short?: boolean,
+	// Sort objects' keys in alphabetical order (true), or supply a lambda for custom sorting. default: false
 	// If you supply a lambda to the sort option, it will be passed three values: the (string) name of the key, the associated value, and the object being sorted, e.g. { sort:->(key,value,hash){ Float(value) rescue Float::MAX } }
-	aligned?: boolean, // When wrapping objects, line up the colons (per object)? default: false
-	precision?: number, // Decimal precision for non-integer numbers. default: undefined
+	sort?: boolean | ( ( a: string, b: string ) => number ),
+	// When wrapping objects, line up the colons (per object)? default: false
+	aligned?: boolean,
+	// Decimal precision for non-integer numbers. default: undefined
+	precision?: number,
 	
-	padding?: number, // Number of spaces to put inside brackets and braces. default: 0
-	arrayPadding?: number, // Number of spaces to put inside brackets for arrays. default: 0
-	objectPadding?: number, // Number of spaces to put inside braces for objects. default: 0
+	// Number of spaces to put inside brackets for arrays and braces for objects. default: 1
+	padding?: number,
+	// Number of spaces to put inside brackets for arrays. default: 1
+	arrayPadding?: number,
+	// Number of spaces to put inside braces for objects. default: 1
+	objectPadding?: number,
 	
-	aroundComma?: number, // Number of spaces to put before and after commas (for both arrays and objects). default: 0
-	beforeComma?: number, // Number of spaces to put before commas (for both arrays and objects). default: 0
-	afterComma?: number, // Number of spaces to put after commas (for both arrays and objects). default: 0
+	// Number of spaces to put before and after commas (for both arrays and objects). default: undefined
+	aroundComma?: number,
+	// Number of spaces to put before commas (for both arrays and objects). default: 0
+	beforeComma?: number,
+	// Number of spaces to put after commas (for both arrays and objects). default: 1
+	afterComma?: number,
 	
-	aroundColon?: number, // Number of spaces before and after a colon. default: 0
-	beforeColon?: number, // Number of spaces before a colon. default: 0
-	afterColon?: number, // Number of spaces after a colon. default: 0
+	// Number of spaces before and after a colon. default: 0
+	aroundColon?: number,
+	// Number of spaces before a colon. default: 0
+	beforeColon?: number,
+	// Number of spaces after a colon. default: 1
+	afterColon?: number,
 	
-	aroundColon1?: number, // Number of spaces before and after a colon when the object is on one line. default: 0
-	beforeColon1?: number, //Number of spaces before a colon when the object is on one line. default: 0
-	afterColon1?: number, // Number of spaces after a colon when the object is on one line. default: 0
+	// Number of spaces before and after a colon when the object is on one line. default: 0
+	aroundColon1?: number,
+	//Number of spaces before a colon when the object is on one line. default: 0
+	beforeColon1?: number,
+	// Number of spaces after a colon when the object is on one line. default: 1
+	afterColon1?: number,
 	
-	aroundColonN?: number, // Number of spaces before and after a colon when the object is on multiple lines. default: 0
-	beforeColonN?: number, // Number of spaces before a colon when the object is on multiple lines. default: 0
-	afterColonN?: number // Number of spaces after a colon when the object is on multiple lines. default: 0
+	// Number of spaces before and after a colon when the object is on multiple lines. default: 0
+	aroundColonN?: number,
+	// Number of spaces before a colon when the object is on multiple lines. default: 0
+	beforeColonN?: number,
+	// Number of spaces after a colon when the object is on multiple lines. default: 1
+	afterColonN?: number
 };
 
 export default function neatJSON( value, opts: Options = {} ) {
 	if ( !( 'wrap' in opts ) ) opts.wrap = 80;
 	if ( opts.wrap === true ) opts.wrap = -1;
-	if ( !( 'indent' in opts ) ) opts.indent = '  ';
-	if ( !( 'arrayPadding' in opts ) ) opts.arrayPadding = opts.padding ?? 0;
-	if ( !( 'objectPadding' in opts ) ) opts.objectPadding = opts.padding ?? 0;
+	if ( !( 'indent' in opts ) ) opts.indent = '\t';
+	if ( !( 'newline' in opts ) ) opts.newline = os.EOL;
+	if ( !( 'arrayPadding' in opts ) ) opts.arrayPadding = opts.padding ?? 1;
+	if ( !( 'objectPadding' in opts ) ) opts.objectPadding = opts.padding ?? 1;
 	if ( !( 'beforeComma' in opts ) ) opts.beforeComma = opts.aroundComma ?? 0;
-	if ( !( 'afterComma' in opts ) ) opts.afterComma = opts.aroundComma ?? 0;
+	if ( !( 'afterComma' in opts ) ) opts.afterComma = opts.aroundComma ?? 1;
 	if ( !( 'beforeColon' in opts ) ) opts.beforeColon = opts.aroundColon ?? 0;
-	if ( !( 'afterColon' in opts ) ) opts.afterColon = opts.aroundColon ?? 0;
+	if ( !( 'afterColon' in opts ) ) opts.afterColon = opts.aroundColon ?? 1;
 	if ( !( 'beforeColon1' in opts ) ) opts.beforeColon1 = opts.aroundColon1 ?? opts.beforeColon ?? 0;
 	if ( !( 'afterColon1' in opts ) ) opts.afterColon1 = opts.aroundColon1 ?? opts.afterColon ?? 0;
 	if ( !( 'beforeColonN' in opts ) ) opts.beforeColonN = opts.aroundColonN ?? opts.beforeColon ?? 0;
@@ -72,7 +99,7 @@ export default function neatJSON( value, opts: Options = {} ) {
 					return `${indent}"NaN"`;
 				} else {
 					const isFloat = o === +o && o !== ( o | 0 );
-					return indent + ( isFloat && opts.precision === undefined ? `${o}` : o.toFixed( +opts.precision ) );
+					return `${indent}${isFloat && opts.precision === undefined ? `${o}` : o.toFixed( +opts.precision )}`;
 				}
 			} else if ( o instanceof Array ) {
 				if ( !o.length ) return `${indent}[]`;
@@ -83,12 +110,12 @@ export default function neatJSON( value, opts: Options = {} ) {
 					const indent2 = `${indent} ${apad}`;
 					pieces = o.map( ( v ) => build( v, indent2 ) );
 					pieces[ 0 ] = pieces[ 0 ].replace( indent2, `${indent}[${apad}` );
-					pieces[ pieces.length - 1 ] = `${pieces[ pieces.length - 1 ] + apad}]`;
-					return pieces.join( ',\n' );
+					pieces[ pieces.length - 1 ] = `${pieces[ pieces.length - 1 ]}${apad}]`;
+					return pieces.join( `,${opts.newline}` );
 				} else {
-					const indent2 = indent + opts.indent;
+					const indent2 = `${indent}${opts.indent}`;
 					return `${indent}[
-${o.map( ( v ) => build( v, indent2 ) ).join( ',\n' )}
+${o.map( ( v ) => build( v, indent2 ) ).join( `,${opts.newline}` )}
 ${opts.indentLast ? indent2 : indent}]`;
 				}
 			} else if ( o instanceof Object ) {
@@ -122,15 +149,15 @@ ${opts.indentLast ? indent2 : indent}]`;
 					}
 					const wrappedKeyVals = keyVals.map( ( [ k, v ] ) => {
 						const indent2 = ' '.repeat( ( k + colonN ).length );
-						oneLine = k + colonN + build( v, '' );
+						oneLine = `${k}${colonN}${build( v, '' )}`;
 						return oneLine.length <= opts.wrap || !v || typeof v !== 'object'
 							? oneLine
-							: k + colonN + build( v, indent2 ).replace( /^\s+/, '' );
+							: `${k}${colonN}${build( v, indent2 ).replace( /^\s+/, '' )}`;
 					} );
-					return `${wrappedKeyVals.join( ',\n' ) + opad}}`;
+					return `${wrappedKeyVals.join( `,${opts.newline}` )}${opad}}`;
 				} else {
 					const keyVals = sortedKV.map( ( kvs ) => {
-						kvs[ 0 ] = indent + opts.indent + JSON.stringify( kvs[ 0 ] );
+						kvs[ 0 ] = `${indent}${opts.indent}${JSON.stringify( kvs[ 0 ] )}`;
 						return kvs;
 					} );
 					if ( opts.aligned ) {
@@ -141,20 +168,20 @@ ${opts.indentLast ? indent2 : indent}]`;
 						for ( let i = keyVals.length; i--; )
 							keyVals[ i ][ 0 ] = keyVals[ i ][ 0 ].padEnd( longest );
 					}
-					const indent2 = indent + opts.indent;
+					const indent2 = `${indent}${opts.indent}`;
 					
 					const wrappedKeyVals = keyVals.map( ( [ k, v ] ) => {
-						oneLine = k + colonN + build( v, '' );
+						oneLine = `${k}${colonN}${build( v, '' )}`;
 						return oneLine.length <= opts.wrap || !v || typeof v !== 'object'
 							? oneLine
-							: k + colonN + build( v, indent2 ).replace( /^\s+/, '' );
+							: `${k}${colonN}${build( v, indent2 ).replace( /^\s+/, '' )}`;
 					} );
 					return `${indent}{
-${wrappedKeyVals.join( ',\n' )}
+${wrappedKeyVals.join( `,${opts.newline}` )}
 ${opts.indentLast ? indent2 : indent}}`;
 				}
 			} else {
-				return indent + JSON.stringify( o );
+				return `${indent}${JSON.stringify( o )}`;
 			}
 		}
 	}
