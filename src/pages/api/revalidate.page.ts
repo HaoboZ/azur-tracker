e55aad92af9ccf6a9d14c792853e7299b175ca3d
Haobo Zhang <haobozhang9081@gmail.com>
2022-07-14 12:58:59 -0700

    still incorrect revalidate

--- src/pages/api/revalidate.page.ts
+++ src/pages/api/revalidate.page.ts
@@ -2,12 +2,13 @@ import { NextApiHandler } from 'next';
 
 const revalidate: NextApiHandler = async ( req, res ) => {
 	try {
-		await res.revalidate( '' );
+		await res.revalidate( '/' );
 		await res.revalidate( '/research' );
 		await res.revalidate( '/fleet' );
 		await res.revalidate( '/info' );
 		return res.send( 'Success' );
 	} catch ( err ) {
+		console.error( err );
 		return res.status( 500 ).send( 'Error revalidating' );
 	}
 };
