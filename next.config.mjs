c99d0625da228dc623fd9deb0b8a2f920aa81906
Haobo Zhang <haobozhang9081@gmail.com>
2022-10-30 22:09:53 -0700

    not permanant redirect

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
 		headers: [ { key: 'Access-Control-Allow-Origin', value: '*' } ]
 	} ],
 	async redirects() {
-		return [ { source: '/', destination: '/event', permanent: true } ];
+		return [ { source: '/', destination: '/event' } ];
 	},
 	experimental: {
 		modularizeImports: {
