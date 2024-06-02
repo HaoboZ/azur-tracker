7ed71c8977b1acba611905a9dfb458b7fa5cb3b3
Haobo Zhang <haobozhang9081@gmail.com>
2024-06-02 11:00:55 -0700

    test logs

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from './client';
 
 export default new PrismaClient({
-	log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : undefined,
+	log: ['query', 'info', 'warn', 'error'],
 });
